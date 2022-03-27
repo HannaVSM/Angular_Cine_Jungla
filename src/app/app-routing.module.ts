fdcfe8cb14b993d8613fdce119c994c9c2c765d0
Hanna Valentina Sarmiento Márquez <sarmientomarquezhannavalentina@gmail.com>
2022-03-26 19:33:36 -0500

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarteleraComponent } from './components/cartelera/cartelera.component';
+import { CompraComponent } from './components/compra/compra.component';
+import { FuncionPeliculaComponent } from './components/funcion-pelicula/funcion-pelicula.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { PagoComponent } from './components/pago/pago.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { RegistrarseComponent } from './components/registrarse/registrarse.component';
+import { SalaComponent } from './components/sala/sala.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'login',
+  component: IniciarSesionComponent
+},{
+  path:'cartelera',
+  component: CarteleraComponent
+},{
+  path:'compra',
+  component: CompraComponent
+},{
+  path:'funcion-pelicula',
+  component: FuncionPeliculaComponent
+},{
+  path:'pago',
+  component: PagoComponent
+},{
+  path:'perfil',
+  component: PerfilComponent
+},{
+  path:'register',
+  component: RegistrarseComponent
+},{
+  path:'sala',
+  component: SalaComponent
+},{
+   path: '**', redirectTo: 'login'
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports:  [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,30 @@ import { AppComponent } from './app.component';
 import { CompraComponent } from './components/compra/compra.component';
 import { CarteleraComponent } from './components/cartelera/cartelera.component';
 import { FuncionPeliculaComponent } from './components/funcion-pelicula/funcion-pelicula.component';
-import { SalaComponent } from './components/comida/sala.component';
+import { SalaComponent } from './components/sala/sala.component';
 import { PagoComponent } from './components/pago/pago.component';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
-import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
+import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 
 @NgModule({
   declarations: [
-
-  
+    AppComponent,
+    CarteleraComponent,
+    CompraComponent,
+    FuncionPeliculaComponent,
     IniciarSesionComponent,
-         RegistrarseComponent,
-         PerfilComponent
+    PagoComponent,
+    PerfilComponent,
+    RegistrarseComponent,
+    SalaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  exports: [AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
