f2935563b744b3e2a527ad011b4fa509794df4d5
Juan Forero <jeforeror@correo.udistrital.edu.co>
2022-03-28 21:54:25 -0500

    components added

--- src/app/components/cartelera/cartelera.component.ts
+++ src/app/components/cartelera/cartelera.component.ts
@@ -12,4 +12,26 @@ export class CarteleraComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+
 }
+
+// names: string[] = ["Mamá", 'Aleja', 'Santi']
+//   newName = "";
+//   products: Product[] = [
+//     {
+//       name: 'EL mejor juguete',
+//       price: 565,
+//       image: './assets/images/toy.webp',
+//       category: 'all',
+//     },
+//     {
+//       name: 'Bicicleta casi nueva',
+//       price: 356,
+//       image: './assets/images/bike.webp'
+//     },
+//     {
+//       name: 'Colleción de albumnes',
+//       price: 34,
+//       image: './assets/images/album.webp'
+//     },
